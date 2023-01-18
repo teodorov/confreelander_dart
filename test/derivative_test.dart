11592294ebc840648ce77b81657b11c73f47d0e6
Ciprian Teodorov <ciprian.teodorov@gmail.com>
2023-01-18 18:24:42 +0100

    some more tests

--- test/derivative_test.dart
+++ test/derivative_test.dart
@@ -16,5 +16,27 @@ void main() {
       expect(token(23).derive(23), isA<Epsilon>());
       expect(token(23).derive(23).parseTrees(), equals({23}));
     });
+    test('D delta', () {
+      expect(identical(token('42').delta().derive(23), empty), true);
+    });
+
+    test('D delayed', () {
+      expect(Delayed(token(42), 42).derive(42), empty);
+    });
+    test('D delayed a force', () {
+      expect(Delayed(token('a'), 'a').force(), isA<Epsilon>());
+      expect(Delayed(token('a'), 'b').force(), empty);
+    });
+
+    test('D delayed a∘b toString', () {
+      var lang = token('a').concatenation(token('b'));
+      expect(Delayed(lang, 'a').derive('b').toString(),
+          lang.derive('a').derive('b').toString());
+    });
+
+    test('D delayed a∘b equals', () {
+      var lang = token('a').concatenation(token('b'));
+      expect(Delayed(lang, 'a').derive('b'), lang.derive('a').derive('b'));
+    });
   });
 }
