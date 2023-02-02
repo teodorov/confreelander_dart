dd86d806d21102159f9e7f11cb8d4142a3566537
Ciprian Teodorov <ciprian.teodorov@gmail.com>
2023-02-02 12:22:52 +0100

    added a test with abc

--- test/parser_test.dart
+++ test/parser_test.dart
@@ -56,6 +56,16 @@ void main() {
       expect('$parseTree', '{[a, b]}');
     });
 
+    test('abc', () {
+      var s = token('a').seq(token('b').seq(token('c')));
+      var word = 'abc'.characters;
+      expect(s.includes(word), true);
+      var parseTree = s.parse(word);
+      // print('-' * 10 + s.toString());
+      print(parseTree);
+      expect('$parseTree', '{[a, [b, c]]}');
+    });
+
     test('S = S ((t +) S) | (t a) -- a+a', () {
       var rS = ref('S');
       var s = rS.seq(token('+').seq(rS)) | token('a');
